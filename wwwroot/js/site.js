2510f9a1a90ce7d32ba822623b21a666b12a60e6
gals <gal.shemesh177@gmail.com>
2018-11-02 18:17:44 +0200

    Added bing map support to each track page - Liran Glikman

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var BingMapKey = "AlNATsB8l5skcoutGNA8P9tN0RobQpXYatlSexjFkbmEoxV-Nm87VRhW2SqhVqDW";
+
+var renderRequestsMap = function (divIdForMap, requestData) {
+    if (requestData) {
+        var bingMap = createBingMap(divIdForMap);
+        addRequestPins(bingMap, requestData);
+    }
+}
+
+function createBingMap(divIdForMap) {
+    return new Microsoft.Maps.Map(
+        document.getElementById(divIdForMap), {
+            credentials: BingMapKey
+        });
+}
+
+function addRequestPins(bingMap, requestData) {
+    bingMap.setView(
+        {
+            center: new Microsoft.Maps.Location(data.lat, data.long),
+            zoom: 15
+        }
+    );
+}
