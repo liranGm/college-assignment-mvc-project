58657ad083203a3e7df2fc8e811fd8ce6bb80a5f
Barak Gonen <barakgonen9@gmail.com>
2018-11-06 14:46:35 +0200

    Added new property for user's model - role
    * Enum contains admin, guide and user

---
+++ Migrations/20181106124536_added_users_role_enum_to_user_model.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace college_assignment_mvc_project.Migrations
+{
+    public partial class added_users_role_enum_to_user_model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "User",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "User");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace college_assignment_mvc_project.Models
 {
@@ -13,7 +14,17 @@ namespace college_assignment_mvc_project.Models
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string PhoneNumber { get; set; }
-
+    public UserAuthorization Role { get; set; }
     public virtual ICollection<Order> OrderedTrips { get; set; } // each user might have multiple orders
+    }
+
+    public enum UserAuthorization
+    {
+        [Display(Name = "Admin")]
+        ADMIN,
+        [Display(Name = "Guide")]
+        GUIDE,
+        [Display(Name = "User")]
+        USER
     }
 }
